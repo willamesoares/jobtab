01d1921f70ec0de0389d4289aa177880a94b4883
Will Soares <willame_soares@hotmail.com>
2017-08-16 15:24:44 -0300

    Add Redux and create folder structure

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, './src'),
-  entry: ['./scripts/app.js', './styles/main.scss'],
+  entry: ['./scripts/index.js', './styles/main.scss'],
   output: {
     filename: 'app.bundle.js',
     path: path.resolve(__dirname, './dist/assets'),
